880c3a3e986950e2c21c1c68942a3f74425ce38a
Aman Dangol <amandangol423@gmail.com>
2024-05-07 15:01:55 +0545

    displayed data from database

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,16 @@
+let table = document.getElementById("table");
+
 async function getData() {
   let response = await fetch("/getList");
   let data = await response.json();
   console.log(data);
+  data.forEach((row) => {
+    table.innerHTML += `<tr>
+    <td>${row.task}</td>
+    <td>update</td>
+    <td>delete</td>
+    </tr>`;
+  });
 }
 
 getData();
